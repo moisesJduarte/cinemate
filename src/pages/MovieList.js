0f08a577a31aa53fc87c5295de8317f931e83dda
Moises Duarte <gladmd19@gmail.com>
2024-09-09 23:00:55 -0400

    initialize project, components, routes, Api, tailwind confi

---
+++ src/pages/MovieList.js
@@ -0,0 +1,9 @@
+export const MovieList = () => {
+    return (
+        <main>
+            <div className="text-9xl bg-emerald-200 p-10  border-8 border-blue-800 rounded-lg m-5">
+                Movie List
+            </div>
+        </main>
+    )
+}
---
+++ src/routes/Allroutes.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import { MovieList, MovieDetail, Search, PageNotFound } from "../pages";
+
+import React from 'react'
+
+export const Allroutes = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<MovieList />} />
+                <Route path="movie/:id" element={<MovieDetail />} />
+                <Route path="movies/popular" element={<MovieList />} />
+                <Route path="movies/top" element={<MovieList />} />
+                <Route path="movies/upcoming" element={<MovieList />} />
+                <Route path="search" element={<Search />} />
+                <Route path="*" element={<PageNotFound />} />
+
+
+
+            </Routes>
+
+        </>
+    )
+}
